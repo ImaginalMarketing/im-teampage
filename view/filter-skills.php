4b618a41f4842b2cae71febb7d50a66ad3c964b5
ImaginalMarketing <coreylavender@Corey-Lavender.local>
2017-03-14 14:49:18 -0500

    Reworked the Skills filter.

--- view/filter-skills.php
+++ view/filter-skills.php
@@ -1,39 +1,21 @@
 <?php
-// declare your shit
-$slugs = array();
-$titles = array();
-$terms = array();
-$i = 0;
+wp_reset_query();
 // get titles + slugs, then push to their arrays
 $args = array(
 		'orderby'       => 'slug', 
 		'order'         => 'DESC',
 		);
 $getterms = get_terms( 'team_skills', $args );
+echo '<ul id="stylistSort">';
 if ( ! empty( $getterms ) && ! is_wp_error( $getterms ) ){
 	foreach ( $getterms as $getterm ) {
-        $i++;
-        $slugs[] = $getterm->slug;
-        $titles[] = $getterm->name;
-        // stack slugs for Isotope
-        $output = array_slice($slugs, 0, $i);
-        array_push($terms, $output);
+        $slug = $getterm->slug;
+        $title = $getterm->name;
+		echo '<li class="' . $slug . '"><label><input name="service_cat" type="radio" value="' . $slug . '">' . $title . '</label></li>';
 	}
-    $terms = array_reverse($terms);
-    $titles = array_reverse($titles);
 }
 
 // put it all together
-echo '<ul id="stylistSort">';
-$x = -1;
-foreach ($titles as $title) {
-    // count up, parse slugs, print filter
-    $x++;
-    $slug = implode(",", $terms[$x]);
-    echo '<li class="hair"><label><input name="service_cat" type="radio" value="' . $slug . '">' . $title . '</label></li>';
-}
 echo '</ul>';
 
 ?>
-
-
