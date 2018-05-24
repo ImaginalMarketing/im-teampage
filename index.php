fa2b1b44e66f04ba98d0cede05568868ae417f7a
Michael M <info@mode87.com>
2018-05-24 17:16:50 -0500

    update ver # lololol

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ Plugin Name: IM Team Page
 Plugin URI: http://imaginalmarketing.com
 Description: A custom post type for staff/team/serfs with filters and stuff. Execute with <strong>[im-teampage]</strong> or <strong>[im-teampage location='slug']</strong>
 Author: Imaginal Marketing
-Version: 0.2
+Version: 0.2.1
 Author URI: http://imaginalmarketing.com
 */
 
